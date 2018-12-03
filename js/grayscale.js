0374a834335eb55f3c7e6372fabcbab9b4220957
chanaa <mohammedchanaa@hotmail.com>
2018-12-03 18:19:09 +0100

    ex

--- js/grayscale.js
+++ js/grayscale.js
@@ -45,6 +45,9 @@
        })
   });
 
+
+
+
   // Team members
   $('.team-member .icon span').hide();
 
@@ -59,3 +62,19 @@
 );
 
 })(jQuery); // End of use strict
+
+
+$('.share-alt').on('click', function(e){
+  $(".branch").removeClass("no-animation");
+  $('.branch').toggleClass("open");
+});
+
+$('.social').on('click', function(e){
+  $(".containerr").removeClass("no-animation");
+  $(".containerr").toggleClass("open");
+});
+
+$('.share').on('click', function(e){
+  $(".float-circle").removeClass("no-animation");
+  $(".float-circle").toggleClass("open");
+});
